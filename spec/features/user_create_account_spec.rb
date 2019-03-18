ff78e7de4bc6fbee7263b75beda53f155213283a
Luis Arantes <luis.arantes.sp@gmail.com>
2019-03-18 00:26:01 -0300

    Add tests for creation of Users

---
+++ spec/features/user_create_account_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+feature 'User create account' do
+  scenario 'successfully' do
+    visit root_path
+    click_on 'Criar nova Conta'
+
+    fill_in 'Nome', with: 'Tobias'
+    fill_in 'Email', with: 'tobias@avantasia.com'
+    fill_in 'Senha', with: 'thescarecrow2008'
+    fill_in 'Confirmar Senha', with: 'thescarecrow2008'
+    click_on 'Enviar'
+
+    expect(page).to have_content('Logado como: tobias@avantasia.com')
+    expect(page).not_to have_link('Criar nova Conta')
+    expect(page).to have_link('Sair')
+  end
+
+  scenario 'and do not fill fields' do
+    visit root_path
+    click_on 'Criar nova Conta'
+
+    fill_in 'Nome', with: ''
+    fill_in 'Email', with: ''
+    fill_in 'Senha', with: ''
+    fill_in 'Confirmar Senha', with: ''
+    click_on 'Enviar'
+
+    expect(page).to have_content('Não foi possível criar a conta')
+  end
+end
