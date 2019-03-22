39f11c562645ca1112c11760adda4142921bdc05
Luis Arantes <luis.arantes.sp@gmail.com>
2019-03-22 18:38:20 -0300

    Add group users viewing tests

---
+++ spec/features/user_views_group_participantes_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+feature 'User views Group Participants' do
+  scenario 'navigating from home' do
+    group_creator = create(:user)
+    group = create(:group, user: group_creator, name: 'Grupo da Faculdade')
+    create(:group_participation, user: group_creator, group: group)
+    participants = []
+    5.times {participants.push(create(:user))}
+    participants.each do |participant|
+      create(:group_participation, user: participant, group: group)
+    end
+    login_as(group_creator, scope: user)
+
+    visit root_path
+    click_on 'Ver grupos criados por mim'
+    click_on group.name
+    
+    expect(page).to have_css('h3', text: 'Participantes do Grupo')
+    expect(page).to have_content(group_creator.name)
+    expect(page).to have_content(participants[0].name)
+    expect(page).to have_content(participants[1].name)
+    expect(page).to have_content(participants[2].name)
+    expect(page).to have_content(participants[3].name)
+    expect(page).to have_content(participants[4].name)
+    expect(page).to have_content('Total de Participantes: 6')
+    expect(page).to have_link('Adicionar Participantes')
+  end
+
+  scenario 'without adding any participant' do
+    group_creator = create(:user)
+    group = create(:group, user: group_creator, name: 'Grupo da Faculdade')
+    create(:group_participation, user: group_creator, group: group)
+    login_as(group_creator, scope: user)
+
+    visit root_path
+    click_on 'Ver grupos criados por mim'
+    click_on group.name
+
+    expect(page).to have_content('Total de Participantes: 1')
+    expect(page).to have_link('Adicionar Participantes')
+  end
+end
