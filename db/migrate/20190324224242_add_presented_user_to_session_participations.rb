fc0e123690aec67a1636e5b815996849a7c1dda4
Luis Arantes <luis.arantes.sp@gmail.com>
2019-03-25 00:55:11 -0300

    Alter PresentedUserMigration - Heroku/PostgreSQL
      makes migration use pure SQL to work on Heroku and PostgreSQL

--- db/migrate/20190324224242_add_presented_user_to_session_participations.rb
+++ db/migrate/20190324224242_add_presented_user_to_session_participations.rb
@@ -1,5 +1,13 @@
 class AddPresentedUserToSessionParticipations < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :session_participations, :presented_user, class_name: 'User', foreign_key: true, optional: true
+  def up 
+    execute "ALTER TABLE session_participations ADD COLUMN presented_user_id INTEGER NULL REFERENCES users (id);"
   end
+
+  def down 
+    execute "ALTER TABLE session_participations DROP COLUMN IF EXISTS presented_user_id;"
+  end
+
+  #def change
+  #  add_reference :session_participations, :presented_user, class_name: 'User', foreign_key: true, optional: true
+  #end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 2019_03_24_224242) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "presented_user_id"
-    t.index ["presented_user_id"], name: "index_session_participations_on_presented_user_id"
     t.index ["session_id"], name: "index_session_participations_on_session_id"
     t.index ["user_id"], name: "index_session_participations_on_user_id"
   end
