346763a42595e506d0dfa970476145d5a047a3f4
Luis Arantes <luis.arantes.sp@gmail.com>
2019-03-24 21:19:28 -0300

    Add inclusion of participants to sessions tests

--- spec/features/user_creates_new_session_for_group_spec.rb
+++ spec/features/user_creates_new_session_for_group_spec.rb
@@ -51,4 +51,54 @@ feature 'User creates new session for group' do
     expect(page).to have_content('Você precisa dar um nome para a Sessão')
     expect(Session.count).to eq(0)
   end
+
+  scenario 'and views session participants' do
+    user = create(:user)
+    user2 = create(:user)
+    user3 = create(:user)
+    user4 = create(:user)
+    group = create(:group, name: 'Turma do D&D', user: user)
+    create(:group_participation, user: user, group: group)
+    create(:group_participation, user: user2, group: group)
+    create(:group_participation, user: user3, group: group)
+    create(:group_participation, user: user4, group: group)
+    login_as(user, scope: :user)
+
+    visit root_path
+    click_on 'Ver meus grupos'
+    click_on group.name
+    click_on 'Ver sessões do Grupo'
+    click_on 'Criar nova Sessão'
+    fill_in 'Nome', with: 'Sorteio Anime Friends'
+    click_on 'Confirmar'
+    click_on Session.last.name
+    
+    expect(page).to have_content(user.name)
+    expect(page).to have_content(user2.name)
+    expect(page).to have_content(user3.name)
+    expect(page).to have_content(user4.name)
+    expect(page).to have_content('Total de Participantes: 4')
+  end
+
+  scenario 'only if group has more than three participants' do
+    user = create(:user)
+    user2 = create(:user)
+    group = create(:group, name: 'Turma do D&D', user: user)
+    create(:group_participation, user: user, group: group)
+    create(:group_participation, user: user2, group: group)
+    login_as(user, scope: :user)
+    
+    visit root_path
+    click_on 'Ver meus grupos'
+    click_on group.name
+    click_on 'Ver sessões do Grupo'
+    click_on 'Criar nova Sessão'
+    fill_in 'Nome', with: 'Sorteio Campus Party'
+    click_on 'Confirmar'
+    click_on Session.last.name
+
+    expect(page).to have_content('Não foi possível criar a Sessão')
+    expect(page).to have_content('É necessário haver no mínimo três pessoas participando de um grupo para criar uma Sessão')
+    expect(Session.count).to eq(0)
+  end
 end
