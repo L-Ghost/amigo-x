ea3a189c508e9c12c1c8f442f9173093105f67ed
Luis Arantes <luis.arantes.sp@gmail.com>
2019-03-24 23:13:21 -0300

    Fills seeds file with basic application data
      seeds files adds five users, and creates a group with all these users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+MAIN_EMAIL = 'user@email.com'
+users = [
+    ['User 1', MAIN_EMAIL],
+    ['User 2', 'user2@email.com'],
+    ['User 3', 'user3@email.com'],
+    ['User 4', 'user4@email.com'],
+    ['User 5', 'user5@email.com']
+]
+
+users.each do |name, email|
+  if !User.where(email: email).first
+    User.create(name: name, email: email, password: '123456')
+  end
+end
+
+main_user = User.where(email: MAIN_EMAIL).first
+
+if !Group.where(name: 'Grupo Teste', user: main_user).first
+  group = Group.create(name: 'Grupo Teste', user: main_user)
+  users.each do |name, email|
+    user = User.where(email: email).first
+    GroupParticipation.create(group: group, user: user)
+  end
+end
